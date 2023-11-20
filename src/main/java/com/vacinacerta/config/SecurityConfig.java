5c3e3ea3c54d14b757313c8a2dc6b6e666651168
Jayv1s <joaovsgcontato@gmail.com>
2023-11-20 17:01:59 -0300

    feature(bean): added security beans and route validation

--- src/main/java/com/vacinacerta/config/SecurityConfig.java
+++ src/main/java/com/vacinacerta/config/SecurityConfig.java
@@ -1,2 +1,44 @@
-package com.vacinacerta.config;public class SecurityConfig {
+package com.vacinacerta.config;
+
+import com.vacinacerta.usecase.RequestFilter;
+import com.vacinacerta.usecase.UserDetailsServiceImpl;
+import com.vacinacerta.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public OncePerRequestFilter requestFilterJwt() {
+        return new RequestFilter(jwtUtils, userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/vaccine/**").hasAnyRole("ADMIN", "USER")
+                    .antMatchers("/users/vaccines").hasAnyRole("ADMIN", "USER")
+                    .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
+                .and()
+                    .sessionManagement()
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .addFilterBefore(requestFilterJwt(), UsernamePasswordAuthenticationFilter.class);
+    }
 }
