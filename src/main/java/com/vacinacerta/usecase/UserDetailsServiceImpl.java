d96a0250a95ad2d3c30e5954406b486d2587039f
Jayv1s <joaovsgcontato@gmail.com>
2023-11-20 17:00:36 -0300

    feature(userdetails): added userDetailsServiceImpl

--- src/main/java/com/vacinacerta/usecase/UserDetailsServiceImpl.java
+++ src/main/java/com/vacinacerta/usecase/UserDetailsServiceImpl.java
@@ -1,2 +1,30 @@
-package com.vacinacerta.usecase;public class UserDetailsServiceImp {
+package com.vacinacerta.usecase;
+
+import com.entities.db.UserAccess;
+import com.vacinacerta.repository.IUserAccessRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private IUserAccessRepository userAccessRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        UserAccess userAccess = userAccessRepository.findById(login).orElseThrow(() -> new UsernameNotFoundException("Login não encontrado."));
+
+        String[] roles = userAccess.getRoles().split(",");
+
+        return User.builder()
+                .username(userAccess.getLogin())
+                .password(userAccess.getPassword())
+                .roles(roles)
+                .build();
+    }
 }
